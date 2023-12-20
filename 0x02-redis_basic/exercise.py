a2cbee561ac311980e77002cfeef81715627ae68
stephenkiai <stephenkiai23@gmail.com>
2023-12-21 01:20:39 +0300

    make executable

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,33 +1,36 @@
+#!/usr/bin/env python3
+""" Define a class 'cache' """
 import redis
-import uuid
 from typing import Union, Callable
+import uuid
 
 
 class Cache:
-    """A class to handle caching using Redis."""
-
+    """ create a class cache """
     def __init__(self):
-        """Initialize the cache instance &
-        flush the Redis database."""
+        """ initialize the cache instace """
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Generate random key using uuid,
-        store data in Redis,return the key."""
+        """ A menthod generating random key using uuid
+        """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Callable = None):
-        """Retrieve values from Redis."""
+    def get(self, key: str,
+            fn: callable = None
+            ):
+        """ retrives values from redis """
         data = self._redis.get(key)
-        return fn(data) if fn and data is not None else data
+        if data is not None:
+            return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> str:
-        """Get data and convert to string."""
+        """ get data and converts to string """
         return self.get(key, fn=lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        """Get data and convert to integer."""
-        return self.get(key, fn=lambda x: int(x))
+        """ get data and converts to integer """
+        return self.get(key, fn = lambda x: int(x))
